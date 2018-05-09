50c734d04a17ea77106af53e00b93deabc5d3a06
Dom Vinyard <domvinyard@me.com>
2018-05-09 17:36:38 +0100

    remove index

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 import React from "react";
-import { render } from "react-dom";
-import Hello from "./Hello";
-import OnScroll from "./React-onscroll";
+import EventListener, { withOptions } from "react-event-listener";
 
-const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center",
-  height: "8000px"
-};
+export default class ReactOnScroll extends React.Component {
+  state = {
+    pixels: 0,
+    percent: 0
+  };
 
-const App = () => (
-  <OnScroll>
-    {scroll => (
-      <div style={styles}>
-        <h2 style={{ position: "fixed", top: 0 }}>{JSON.stringify(scroll)}</h2>
+  componentDidMount() {
+    this.handleScroll();
+  }
+
+  handleScroll() {
+    const h = document.documentElement,
+      b = document.body,
+      st = "scrollTop",
+      sh = "scrollHeight";
+
+    const percentPrecision =
+      (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+    const percent = parseFloat(
+      (Math.round(percentPrecision * 100) / 100).toFixed(2)
+    );
+    this.setState({
+      pixels: document.body[st],
+      percent
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <EventListener target="window" onScroll={() => this.handleScroll()} />
+        <div>{this.props.children(this.state)}</div>
       </div>
-    )}
-  </OnScroll>
-);
-
-render(<App />, document.getElementById("root"));
+    );
+  }
+}
